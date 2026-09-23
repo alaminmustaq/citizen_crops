f247e27352f9f179d0e0e04c032befd17e637a42
Al Aamin <alaminmushtaq321@gmail.com>
2026-09-23 11:26:13 +0600

    server push

--- components/partials/footer/index.jsx
+++ components/partials/footer/index.jsx
@@ -7,94 +7,85 @@ import FooterLayout from "./footer-layout";
 import { useMounted } from "@/hooks/use-mounted";
 
 const Footer = ({ handleOpenSearch }) => {
-  const { collapsed, sidebarType } = useSidebar();
-  const { layout, footerType } = useThemeStore();
-  const mounted = useMounted();
-  const isMobile = useMediaQuery("(min-width: 768px)");
+    const { collapsed, sidebarType } = useSidebar();
+    const { layout, footerType } = useThemeStore();
+    const mounted = useMounted();
+    const isMobile = useMediaQuery("(min-width: 768px)");
 
-  if (!mounted) {
-    return null;
-  }
-  if (!isMobile && sidebarType === "module") {
-    return <MobileFooter handleOpenSearch={handleOpenSearch} />;
-  }
+    if (!mounted) {
+        return null;
+    }
+    if (!isMobile && sidebarType === "module") {
+        return <MobileFooter handleOpenSearch={handleOpenSearch} />;
+    }
 
-  if (footerType === "hidden") {
-    return null;
-  }
+    if (footerType === "hidden") {
+        return null;
+    }
 
-  if (layout === "semibox") {
+    if (layout === "semibox") {
+        return (
+            <div className="xl:mx-20 mx-6">
+                <FooterLayout
+                    className={cn(" rounded-md border", {
+                        "xl:ltr:ml-[72px] xl:rtl:mr-[72px]": collapsed,
+                        "xl:ltr:ml-[272px] xl:rtl:mr-[272px]": !collapsed,
+                        "sticky bottom-0": footerType === "sticky",
+                    })}
+                >
+                    <FooterContent />
+                </FooterLayout>
+            </div>
+        );
+    }
+    if (sidebarType !== "module" && layout !== "horizontal") {
+        return (
+            <FooterLayout
+                className={cn("", {
+                    "xl:ltr:ml-[248px] xl:rtl:mr-[248px]": !collapsed,
+                    "xl:ltr:ml-[72px] xl:rtl:mr-[72px]": collapsed,
+                    "sticky bottom-0": footerType === "sticky",
+                })}
+            >
+                <FooterContent />
+            </FooterLayout>
+        );
+    }
+
+    if (layout === "horizontal") {
+        return (
+            <FooterLayout
+                className={cn("", {
+                    "sticky bottom-0": footerType === "sticky",
+                })}
+            >
+                <FooterContent />
+            </FooterLayout>
+        );
+    }
+
     return (
-      <div className="xl:mx-20 mx-6">
         <FooterLayout
-          className={cn(" rounded-md border", {
-            "xl:ltr:ml-[72px] xl:rtl:mr-[72px]": collapsed,
-            "xl:ltr:ml-[272px] xl:rtl:mr-[272px]": !collapsed,
-            "sticky bottom-0": footerType === "sticky",
-          })}
+            className={cn("", {
+                "xl:ltr:ml-[300px] xl:rtl:mr-[300px]": !collapsed,
+                "xl:ltr:ml-[72px] xl:rtl:mr-[72px]": collapsed,
+                "sticky bottom-0": footerType === "sticky",
+            })}
         >
-          <FooterContent />
+            <FooterContent />
         </FooterLayout>
-      </div>
     );
-  }
-  if (sidebarType !== "module" && layout !== "horizontal") {
-    return (
-      <FooterLayout
-        className={cn("", {
-          "xl:ltr:ml-[248px] xl:rtl:mr-[248px]": !collapsed,
-          "xl:ltr:ml-[72px] xl:rtl:mr-[72px]": collapsed,
-          "sticky bottom-0": footerType === "sticky",
-        })}
-      >
-        <FooterContent />
-      </FooterLayout>
-    );
-  }
-
-  if (layout === "horizontal") {
-    return (
-      <FooterLayout
-        className={cn("", {
-          "sticky bottom-0": footerType === "sticky",
-        })}
-      >
-        <FooterContent />
-      </FooterLayout>
-    );
-  }
-
-  return (
-    <FooterLayout
-      className={cn("", {
-        "xl:ltr:ml-[300px] xl:rtl:mr-[300px]": !collapsed,
-        "xl:ltr:ml-[72px] xl:rtl:mr-[72px]": collapsed,
-        "sticky bottom-0": footerType === "sticky",
-      })}
-    >
-      <FooterContent />
-    </FooterLayout>
-  );
 };
 
 export default Footer;
 
 const FooterContent = () => {
-  return (
-    <div className="block md:flex md:justify-between text-muted-foreground">
-      <p className="sm:mb-0 text-xs md:text-sm">
-        COPYRIGHT © {new Date().getFullYear()} DashTail All rights Reserved
-      </p>
-      <p className="mb-0 text-xs md:text-sm">
-        Hand-crafted & Made by{" "}
-        <a
-          className="text-primary"
-          target="__blank"
-          href="https://codeshaper.net"
-        >
-          Codeshaper
-        </a>
-      </p>
-    </div>
-  );
+    return (
+        <div className="block md:flex md:justify-between text-muted-foreground">
+            <p className="sm:mb-0 text-xs md:text-sm">
+                COPYRIGHT © {new Date().getFullYear()} DATADSS All rights
+                Reserved
+            </p>
+        </div>
+    );
 };
